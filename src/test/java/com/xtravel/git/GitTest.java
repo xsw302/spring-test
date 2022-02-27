6ce1c1b5e8904cba163bf020159d600c71095b5f
shiwei <xieshiwei3@126.com>
2022-02-27 16:21:50 +0800

    idea commit 2

--- src/test/java/com/xtravel/git/GitTest.java
+++ src/test/java/com/xtravel/git/GitTest.java
@@ -1,7 +1,14 @@
 package com.xtravel.git;
 
+import org.junit.jupiter.api.Test;
+
 public class GitTest {
     public static void main(String[] args) {
 
+    }
+
+    @Test
+    public void aa(){
+        System.out.println(1111);
     }
 }
